19119042bafe43781fad5930cbcda89d97096357
tmssnchzprds <tmssnchzprds@hotmail.com>
2021-03-15 22:54:50 +0100

    Se ha creado la rama clasenueva

---
+++ Tomas_SanchezDAW/src/NuevaClase.java
@@ -0,0 +1,18 @@
+/**
+ * project: Tomas_SanchezDAW
+ * package: 
+ * file: NuevaClase.java
+ */
+
+/**
+ * class: NuevaClase.java
+ * @author Tomas Sanchez Paredes
+ * @version 2021.03.15.a
+ */
+public class NuevaClase {
+
+	public static void main(String[] args) {
+		System.out.println("En la rama. Se crea la clase nueva");
+	}
+
+}
