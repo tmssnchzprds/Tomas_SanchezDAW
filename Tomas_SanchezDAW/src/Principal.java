70e3b7bff22c3fdc971dda8f8b05fa284548e585
tmssnchzprds <tmssnchzprds@hotmail.com>
2021-03-15 22:03:38 +0100

    validar cambios en el repositorio local

---
+++ Tomas_SanchezDAW/src/Principal.java
@@ -0,0 +1,18 @@
+/**
+ * project: Tomas_SanchezDAW
+ * package: 
+ * file: Principal.java
+ */
+
+/**
+ * class: Principal.java
+ * @author Tomas Sanchez Paredes
+ * @version 2021.03.15.a
+ */
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+	}
+
+}
