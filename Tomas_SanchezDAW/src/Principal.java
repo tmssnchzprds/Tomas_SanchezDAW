b4fd4337003b073db6c44d6fc646a99cac20c902
tmssnchzprds <tmssnchzprds@hotmail.com>
2021-03-15 22:40:12 +0100

    Líneas en método main

--- Tomas_SanchezDAW/src/Principal.java
+++ Tomas_SanchezDAW/src/Principal.java
@@ -13,6 +13,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
 	}
 
 }
